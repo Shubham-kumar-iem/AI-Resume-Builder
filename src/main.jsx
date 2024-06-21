90fb84579af10c4bb91d6c241055e3bb1e7b5fa7
Shubham Kumar <shubhammuz110203@gmail.com>
2024-06-21 23:47:03 +0530

    initializing project

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import  Home  from './home/index.jsx'
+import  Dashboard  from './dashboard/index.jsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>,
+      }
+    ]
+  },
+  {
+    path: 'auth/sign-in',
+    element: <SignInPage/>,
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
